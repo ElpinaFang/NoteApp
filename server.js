d487d019d3f0aa91ce1b4578cb2b64830037d5fd
Elpina <elpinaoc@gmail.com>
2025-05-02 12:59:46 +1200

    Add dotenv and variables

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const path = require('path');
 const app = express();
 
 // 🔥 Connect to MongoDB
-mongoose.connect('mongodb+srv://elpinaoc:Laoshu%40111084@clusternote.korupbu.mongodb.net/notesapp?retryWrites=true&w=majority&appName=ClusterNote', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-.then(() => console.log('✅ Connected to MongoDB'))
-.catch(err => console.error('❌ MongoDB connection error:', err));
+.then(() => console.log("✅ Connected to MongoDB"))
+.catch(err => console.error("❌ DB error:", err));
 
 // 🧱 Define a Note model
 const Note = mongoose.model('Note', {
